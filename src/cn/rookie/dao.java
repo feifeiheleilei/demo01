048d66140d056ab4f5c7e77c01dc105cf35ef9a9
feifeiheleilei <1799938430@qq.com>
2016-01-12 18:24:51 +0800

    ee

---
+++ src/cn/rookie/dao.java
@@ -0,0 +1,11 @@
+package cn.rookie;
+
+/**
+ * Created by 201601120071 on 2016/1/12.
+ */
+public class dao {
+    public static void main(String[] args) {
+        System.out.println("aa");
+        int a = 1;
+    }
+}
---
+++ src/cn/rookie/vo/Person.java
@@ -0,0 +1,25 @@
+package cn.rookie.vo;
+
+/**
+ * Created by 201601120071 on 2016/1/12.
+ */
+public class Person {
+    private String name;
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
